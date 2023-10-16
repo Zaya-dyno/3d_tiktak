db550d631822e13e087e82a871d39d003bdadbe9
tuvshinzaya erdenekhuu <tuvshinzaya.erdenekhuu@arm.com>
2023-10-16 11:31:27 +0100

    now only one layer of board has color to be highlighted. and line can be selected by Arrow up and down

--- Assets/script/placeholder.cs
+++ Assets/script/placeholder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using Unity.VisualScripting;
+using System.Drawing;
 
 public class Placeholder : MonoBehaviour
 {
@@ -15,116 +16,15 @@ public class Placeholder : MonoBehaviour
     public Material materials_line;
     public float radius_line;
     static public int side = 4;
+    public int height;
+    public int old_height;
     public float dis = 0.25F;
     GameObject[,,] gameArea;
-
-    public class State
-    {
-        public int[,,] board;
-        public bool changed;
-
-        public State()
-        {
-            board = new int[side, side, side];
-            changed = false;
-        }
-
-        public int place_one((int, int, int) place)
-        {
-            return placement(1,place);
-        }
-
-        public int place_two((int, int, int) place)
-        {
-            return placement(2, place);
-        }
-
-        private int placement(int player, (int,int,int) place)
-        {
-            if (board[place.Item1,place.Item2,place.Item3] == 0)
-            {
-                board[place.Item1, place.Item2, place.Item3] = player;
-                return 0;
-            } 
-            return -1;
-        }
-
-        public int check_win()
-        {
-            //check straight lines
-            for(int i = 0; i < 3; i++)
-            {
-                int[] place = new int[3];
-
-                for(int j = 0; j < side; j++)
-                {
-                    place[i % 3] = j;
-                    for(int z = 0; z < side; z++)
-                    {
-                        place[(i + 1) % 3] = z;
-                        place[(i + 2) % 3] = 0;
-                        int player = board[place[0], place[1], place[2]];
-                        if (player == 0)
-                        {
-                            continue;
-                        }
-                        for(int y = 1; y < side; y++)
-                        {
-                            place[(i + 2) % 3] = y;
-                            if (player != board[place[0], place[1], place[2]])
-                            {
-                                break;
-                            }
-                            if (y == side - 1)
-                            {
-                                return player;
-                            }
-                        }
-                    }
-                }
-            }
-            //check diagonals
-            for(int j = 0; j < 2; j++)
-            {
-                for (int z = 0; z < 2; z++)
-                {
-                    int[] loc;
-                    loc = new int[]{ 0, (side - 1) * j, (side - 1) * z};
-                    int player = board[loc[0], loc[1], loc[2]];
-                    if (player == 0)
-                    {
-                        continue;
-                    }
-                    loc[0] += 1;
-                    int add_1 = 1;
-                    int add_2 = 1;
-                    if (j ==1) { add_1 = -1; }
-                    if (z ==1) { add_1 = -1; }
-                    loc[1] += add_1;
-                    loc[2] += add_2;
-                    for (int index = 1; index < side; index++)
-                    {
-                        if (player != board[loc[0], loc[1], loc[2]])
-                        {
-                            break;
-                        }
-                        loc[0] += 1;
-                        loc[1] += add_1;
-                        loc[2] += add_2;
-                        if (index == side - 1)
-                        {
-                            return player;
-                        }
-                    }
-                }
-            }
-            return 0;
-        }
-    }
 
     // Start is called before the first frame update
     void Start()
     {
+        old_height = -1;
         state = new State();
         Vector3 location;
         gameArea = new GameObject[side,side,side];
@@ -177,6 +77,32 @@ public class Placeholder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (height != old_height)
+        {
+            for(int i = 0; i < side; i++)
+            {
+                float alpha;
+                if (i == height)
+                {
+                    alpha = 100F/255F;
+                } else
+                {
+                    alpha = 0;
+                }
+                UnityEngine.Color color = materials_place[i].color;
+                color.a = alpha;
+                materials_place[i].color = color;
+            }
+            old_height = height;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && height != side - 1)
+        {
+            height += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && height != 0)
+        {
+            height -= 1;
+        }
     }
 }
---
+++ Assets/script/state.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+
+public class State
+{
+    public int[,,] board;
+    public bool changed;
+    public int side = 4;
+
+    public State()
+    {
+        board = new int[side, side, side];
+        changed = false;
+    }
+
+    public int place_one((int, int, int) place)
+    {
+        return placement(1, place);
+    }
+
+    public int place_two((int, int, int) place)
+    {
+        return placement(2, place);
+    }
+
+    private int placement(int player, (int, int, int) place)
+    {
+        if (board[place.Item1, place.Item2, place.Item3] == 0)
+        {
+            board[place.Item1, place.Item2, place.Item3] = player;
+            return 0;
+        }
+        return -1;
+    }
+
+    public int check_win()
+    {
+        //check straight lines
+        for (int i = 0; i < 3; i++)
+        {
+            int[] place = new int[3];
+
+            for (int j = 0; j < side; j++)
+            {
+                place[i % 3] = j;
+                for (int z = 0; z < side; z++)
+                {
+                    place[(i + 1) % 3] = z;
+                    place[(i + 2) % 3] = 0;
+                    int player = board[place[0], place[1], place[2]];
+                    if (player == 0)
+                    {
+                        continue;
+                    }
+                    for (int y = 1; y < side; y++)
+                    {
+                        place[(i + 2) % 3] = y;
+                        if (player != board[place[0], place[1], place[2]])
+                        {
+                            break;
+                        }
+                        if (y == side - 1)
+                        {
+                            return player;
+                        }
+                    }
+                }
+            }
+        }
+        //check diagonals
+        for (int j = 0; j < 2; j++)
+        {
+            for (int z = 0; z < 2; z++)
+            {
+                int[] loc;
+                loc = new int[] { 0, (side - 1) * j, (side - 1) * z };
+                int player = board[loc[0], loc[1], loc[2]];
+                if (player == 0)
+                {
+                    continue;
+                }
+                loc[0] += 1;
+                int add_1 = 1;
+                int add_2 = 1;
+                if (j == 1) { add_1 = -1; }
+                if (z == 1) { add_1 = -1; }
+                loc[1] += add_1;
+                loc[2] += add_2;
+                for (int index = 1; index < side; index++)
+                {
+                    if (player != board[loc[0], loc[1], loc[2]])
+                    {
+                        break;
+                    }
+                    loc[0] += 1;
+                    loc[1] += add_1;
+                    loc[2] += add_2;
+                    if (index == side - 1)
+                    {
+                        return player;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
+
